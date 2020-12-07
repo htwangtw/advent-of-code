902fcd5cb2a6a812f58eddc5a212c637f5b638f0
Hao-Ting Wang <htwangtw@gmail.com>
2020-12-07 22:35:29 +0000

    Day 7 part 1 in recursive

--- 2020/day07.py
+++ 2020/day07.py
@@ -8,18 +8,15 @@ def parse_rules(path):
     for rule in data:
         key = re.search("^([a-z\ ]*) bags contain", rule).group(1)
         contents = re.findall("([0-9]) ([a-z\ ]*) bag", rule)
-        if len(contents) == 0:
-            rules[key] = None
-        else:
-            rules[key] = {c[1]: int(c[0]) for c in contents}
+        rules[key] = {c[1]: int(c[0]) for c in contents}
 
     return rules
 
 rules = parse_rules("2020/data/day07.txt")
 target = "shiny gold"
 
-def outer_bag(target):
-    bags_to_find = {target}
+def outer_bag(color):
+    bags_to_find = {color}
     found_bags = set()
     while bags_to_find:
         find = bags_to_find.pop()
@@ -34,9 +31,25 @@ def outer_bag(target):
 
 print(outer_bag(target))
 
+# part 1 recursive
+def outer_bag(found_bags, bags_to_find):
+    if len(bags_to_find) == 0:
+        return len(found_bags)
+
+    find = bags_to_find.pop()
+    bags = [
+        key
+        for key, content in rules.items()
+        if content and find in content.keys()
+    ]
+    return outer_bag(found_bags.update(bags), bags_to_find.update(bags))
+
+print(outer_bag(set(), {target}))
+
+
 def innner_bags(color):
     contain = rules[color]
-    if contain is None:
+    if len(contain) == 0:
         return 0
     else:
         return sum(contain[key] * innner_bags(key) + contain[key] for key in contain.keys())
