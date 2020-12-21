0fb54f08c3c4157fe8592942dcad9237723356d1
Hao-Ting Wang <htwangtw@gmail.com>
2020-12-21 17:16:18 +0000

    day 20 compile map success

--- 2020/day20.py
+++ 2020/day20.py
@@ -123,7 +123,6 @@ def parse_tiles(data):
         tiles[key] = tile[2:]
     return tiles
 
-
 def flip_tile(tile):
     # original, vertical flip, mirror, mirror verticle flip
     return [tile, tile[::-1],
@@ -131,7 +130,7 @@ def flip_tile(tile):
             [l[::-1] for l in tile][::-1]]
 
 def get_border(tile):
-    # get boarder clockwise
+    # get boarder clockwise from top
     return (
         tile[0],
         "".join(l[-1] for l in tile),
@@ -142,23 +141,10 @@ def get_border(tile):
 def break_tile(tile):
     return [[a for a in l] for l in tile]
 
-def rotate_tile(tile):
-    # top left as the corner, rotate clockwise
-    last  = tile
-    rotate = [["".join(l) for l in tile]]
-    for _ in range(3):
-        tile = [l[:] for l in tile]
-        for x in range(len(tile)):
-            for y in range(len(tile[x])):
-                tile[x][y] = last[len(tile[x])-y-1][x]
-        last = tile
-        rotate.append(["".join(l) for l in tile])
-    return rotate
-
-
 # part 1
-tiles = parse_tiles(data)
+tiles = parse_tiles(test)
 
+# all possible boarder
 boarders = {}
 for key, tile in tiles.items():
     # get boarders
@@ -170,17 +156,205 @@ for key, tile in tiles.items():
     boarders[key] = bds
 
 # find connected tiles
-connected = {}
+connections = {}
 for a, b in product(boarders.keys(), repeat=2):
     if a != b:
-        connections = boarders[a].intersection(boarders[b])
-        if len(connections) > 0:
-            if a in connected:
-                connected[a].append(b)
+        common = boarders[a].intersection(boarders[b])
+        if len(common) > 0:
+            if a in connections:
+                connections[a].append(b)
             else:
-                connected[a] = [b]
+                connections[a] = [b]
 
 # there should be 4 tiles with 2 connections only
 print("part 1:")
-print(prod(key for key, c in connected.items() if len(c) == 2))
+print(prod(key for key, c in connections.items() if len(c) == 2))
 
+
+# part 2
+def rotate_tile(tile):
+    # top left as the corner, rotate 90 degrees cloack wise
+    last  = tile
+    tile = [l[:] for l in tile]
+    for x in range(len(tile)):
+        for y in range(len(tile[x])):
+            tile[x][y] = last[len(tile[x]) - y - 1][x]
+    last = tile
+    return ["".join(l) for l in tile]
+
+def mirror_x(a):
+    # mirror tile bottom to top
+    return a[::-1]
+
+def mirror_y(a):
+    # mirror tile left to right
+    return [l[::-1] for l in a]
+
+# can start -> end achive walking in the edge length only
+def walk_edge(start_tile, sides, path=[]):
+    if len(path) == 0:
+        path = [start_tile]
+
+    buffer = sides.copy()
+    buffer.pop(start_tile)
+    ct = corners.copy()
+    ct.remove(path[0])
+
+    if not buffer:
+        return path
+    candidates = [t for t in connections[start_tile] if t in list(buffer.keys())]
+    path.append(candidates[0])
+    return walk_edge(path[-1], buffer, path)
+
+def fill_edge(edge, tile_map={}):
+    for i, t in enumerate(edge):
+        base = int(i / (grid_size[0] - 1))
+        if base % 2 == 0:
+            x = (grid_size[0] - 1) * int(base / 2)
+            y = -(grid_size[0] - 1) * int(base / 2)
+            if i < grid_size[0]:
+                x += i % (grid_size[0] - 1)
+            else:
+                x -= i % (grid_size[0] - 1)
+        else:
+            x = (grid_size[0] - 1) * (1 - int(base / 2))
+            y = -(grid_size[0] - 1) * int(base / 2)
+            if i < (grid_size[0]*2 -2):
+                y -= i % (grid_size[0] - 1)
+            else:
+                y += i % (grid_size[0] - 1)
+        tile_map[(abs(x), abs(y))] = t
+    return tile_map
+
+def remove_boarder(tile):
+    return [l[1:-1]for l in tile[1:-1]]
+
+def fill_map(start, tile_map, middle):
+    if not middle:
+        return tile_map
+    else:
+        remaining = middle.copy()
+        bl = tile_map[start]
+        br = tile_map[(start[0] + 1, start[1])]
+        tl = tile_map[(start[0], start[1] +1)]
+        target = (start[0] + 1, start[1] + 1)
+        if not tile_map.get(target, False):
+            br = set(connections[br])
+            tl = set(connections[tl])
+            coord = br.intersection(tl)
+            coord.discard(bl)
+            tile_map[target] = coord.pop()
+            remaining.remove(tile_map[target])
+            start = (start[0], start[1] + 1) # new start
+        else:
+            # reach the top
+            start = (start[0] + 1, 0)
+        return fill_map(start, tile_map, remaining)
+
+
+
+n_connection = [len(v) for v in connections.values()]
+print(f"centre tiles: {n_connection.count(4)}")
+print(f"edge tiles: {n_connection.count(3)}")
+print(f"solve: x**2 - {n_connection.count(3) / 2} = {n_connection.count(4)}")
+print(f"solve: y = {n_connection.count(3) / 2} - x")
+
+# I don't know how to solve this in python but I can do it by hand:
+# grid_size = (12, 12)
+grid_size = (3, 3)
+
+# get all the side tiles
+sides = {k: i for k, i in connections.items() if len(i) <= 3}
+corners = [k for k, i in connections.items() if len(i) == 2]
+middle = [k for k, i in connections.items() if len(i) == 4]
+edges = walk_edge(corners[0], sides)
+tile_no_map = fill_edge(edges, tile_map={})
+tile_no_map = fill_map((0, 0), tile_no_map, middle)
+
+
+# find the tile pattern that joins together
+# calibrate one corner (0,0) (three tiles)
+centre = tiles[tile_no_map[(0, 0)]]
+down = tiles[tile_no_map[(0, 1)]]
+right = tiles[tile_no_map[(1, 0)]]
+
+a = boarders[tile_no_map[(0, 0)]].intersection(boarders[tile_no_map[(1, 0)]])
+b = boarders[tile_no_map[(0, 0)]].intersection(boarders[tile_no_map[(0, 1)]])
+
+while get_border(centre)[2] not in b:
+    centre = rotate_tile(break_tile(centre))
+
+if get_border(centre)[1] not in a:
+    centre = mirror_y(centre)
+
+while get_border(right)[-1] not in a:
+    right = rotate_tile(break_tile(right))
+
+if get_border(right)[-1] != get_border(centre)[1]:
+    right = mirror_x(right)
+
+while get_border(down)[0] not in b:
+    down = rotate_tile(break_tile(down))
+
+if get_border(down)[0] != get_border(centre)[2]:
+    down = mirror_y(down)
+
+tiles[tile_no_map[(0, 0)]] = centre
+tiles[tile_no_map[(0, 1)]] = down
+tiles[tile_no_map[(1, 0)]] = right
+
+# fill the map
+tile_map = {}
+prev = None
+prev_coord = None
+for coord, num in sorted(tile_no_map.items()):
+    current = tiles[num]
+    x, y = coord
+    if not prev:
+        prev = current
+        prev_coord = coord
+    elif x == 0: # use bottum of the previsous as ref
+        target = get_border(prev)[2]
+        while get_border(current)[0] not in [target, target[::-1]]:
+            current = rotate_tile(break_tile(current))
+        if get_border(current)[0] == target[::-1]:
+            current = mirror_y(current)
+        prev = current
+        prev_coord = (x, y)
+    else:  # use left as ref
+        prev_coord = (x - 1, y)
+        prev = tiles[tile_no_map[prev_coord]]
+        target = get_border(prev)[1]
+        while get_border(current)[-1] not in [target, target[::-1]]:
+            current = rotate_tile(break_tile(current))
+        if get_border(current)[-1] == target[::-1]:
+            current = mirror_x(current)
+    tiles[num] = current
+    tile_map[coord] = current
+
+for y in range(grid_size[0]):
+    cur_row = [""] * (len(tile) - 2)
+    for x in range(grid_size[0]):
+        for i, l in enumerate(remove_boarder(tile_map[(x, y)])):
+            cur_row[i] = cur_row[i] + l
+    if y == 0:
+        full_map = cur_row
+    else:
+        full_map += cur_row
+
+
+def parse_map(full_map):
+    tmp = break_tile(full_map)
+    full_map = break_tile(full_map)
+    for x in range(len(tmp)):
+        for y in range(len(tmp)):
+            full_map[y][x] = full_map[y][x]=="#"
+    return full_map
+
+
+monster = """                  #
+#    ##    ##    ###
+ #  #  #  #  #  #   """
+
+bin_monster = [[i == "#" for i in l] for l in break_tile(monster.splitlines())]
+bin_map = parse_map(full_map)
